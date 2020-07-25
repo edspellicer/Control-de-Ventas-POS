e4e7ff6aa252de7116e7a9dc10a6e39b9064b1ac
estefania dsantiago <estefaniadsantiago@outlook.com>
2020-07-24 20:35:23 -0400

    Proyecto en Blanco

---
+++ Control de Ventas POS/Control de Ventas POS/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Control_de_Ventas_POS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
