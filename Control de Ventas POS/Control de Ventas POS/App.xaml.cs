26368fb87d034688d3df23fcc4640292b1651560
estefania dsantiago <estefaniadsantiago@outlook.com>
2020-07-24 21:17:53 -0400

    xxxxxxx

--- Control de Ventas POS/Control de Ventas POS/App.xaml.cs
+++ Control de Ventas POS/Control de Ventas POS/App.xaml.cs
@@ -1,28 +1,46 @@
-﻿using System;
+﻿using Control;
+using Control_de_Ventas_POS.Views;
+using System;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Control_de_Ventas_POS
+
+namespace Global
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class App : Application
     {
+        #region Properties
+
+       
+        #endregion
+
+        #region Constructor
+
         public App()
         {
-            InitializeComponent();
-
-            MainPage = new MainPage();
+            //InitializeComponent();
+            MainPage = new MenuPage();
         }
+        #endregion
+
+        #region Methods
 
         protected override void OnStart()
         {
+            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
+            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
+            // Handle when your app resumes
         }
+        #endregion
     }
 }
